50a6e178d1aded4a1992e70a94c23f256bad3722
Marko Krezic <markokrezic90@gmail.com>
2023-04-05 08:47:48 +0200

    Create PodcastItem.vue

---
+++ src/components/PodcastItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="podcast-item" @click="play">
+    {{ podcast.title }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    podcast: {
+      type: Object,
+      required: true,
+   }, 
+  },
+  methods: {
+    play() {
+      this.$emit("play", this.podcast);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.podcast-item {
+  /* Add your podcast item styles here */
+}
+</style>
