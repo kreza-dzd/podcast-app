72c6cb06027b51d30eed04c3f1cba7381313c3f4
Marko Krezic <markokrezic90@gmail.com>
2023-04-05 08:47:46 +0200

    Create MediaPlayer.vue

---
+++ src/components/MediaPlayer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="podcast">
+    <h3>Now Playing: {{ podcast.title }}</h3>
+    <audio :src="podcast.audioFilePath" controls></audio>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    podcast: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your media player styles here */
+</style>
