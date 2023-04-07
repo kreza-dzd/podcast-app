c9834720329986f9682456937880bfd071cde61a
Marko Krezic <markokrezic90@gmail.com>
2023-04-07 09:34:17 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+import App from './App.vue';
 
+library.add(fas);
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
+
