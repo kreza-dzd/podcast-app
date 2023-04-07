6881d529d3c45374b4a5611f6f012bfaadd35197
Marko Krezic <markokrezic90@gmail.com>
2023-04-07 10:29:27 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,30 @@
 
     <main>
       <PodcastList />
+
+  <div class="featured-container">
+  <h2 class="section-title">Featured</h2>
+  <div class="box-wrapper">
+    <div class="box-1"></div>
+    <div class="box-2"></div>
+    <div class="box-3"></div>
+    <div class="box-4"></div>
+    <div class="box-5"></div>
+  </div>
+</div>
+
+
+  <div class="new-container">
+  <h2 class="section-title">New</h2>
+  <div class="box-wrapper">
+    <div class="box-6"></div>
+    <div class="box-7"></div>
+    <div class="box-8"></div>
+    <div class="box-9"></div>
+    <div class="box-10"></div>
+  </div>
+</div>
+
     </main>
   </div>
 </template>
@@ -201,9 +225,92 @@ input[type="text"]::-ms-input-placeholder {
 }
 
 
-.sidebar nav ul li a i {
-  margin-right: 10px;
+.featured-container {
+  margin-top: 50px;
 }
+
+.new-container {
+  margin-top: 50px;
+}
+
+.section-title {
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  margin-left: 10px;
+}
+
+
+
+.box-1, .box-2, .box-3, .box-4, .box-5 {
+  flex: 0 0 70%;
+  margin-right: 20px;
+  background-color: #f8f8f8;
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 100px 30px;
+  text-align: center;
+}
+
+.box-6, .box-7, .box-8, .box-9, .box-10 {
+  flex: 0 0 50%;
+  margin-right: 20px;
+  background-color: #f8f8f8;
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 80px 30px;
+  text-align: center;
+}
+
+.box-1, .box-10 {
+  background-color: #FEF755;
+}
+.box-2, .box-9 {
+  background-color: #83ECCC;
+}
+.box-3, .box-8 {
+  background-color: #A597FF;
+}
+.box-4, .box-7 {
+  background-color: #D17FF8;
+}
+.box-5, .box-6 {
+  background-color: #83D3F5;
+}
+
+
+
+.box-wrapper {
+  display: flex;
+  overflow-x: scroll;
+  margin-bottom: 20px;
+}
+
+
+.featured-box img {
+  width: 80%;
+  margin-bottom: 20px;
+}
+
+.featured-box h2 {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.featured-box p {
+  font-size: 16px;
+  line-height: 1.5;
+  color: #666;
+}
+
 
 
 </style>
