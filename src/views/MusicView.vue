80a4d79e14a2e1eb73d5e5ed47886df2221cd2a3
Marko Krezic <markokrezic90@gmail.com>
2023-04-09 17:05:07 +0100

    Create MusicView.vue

---
+++ src/views/MusicView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Hallo</h1>
+      <p>Welcome to the Music page!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MusicView'
+  };
+  </script>
+  
