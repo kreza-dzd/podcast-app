04db0abbbb2d7c8e77d517bb48a58c52413031da
Marko Krezic <markokrezic90@gmail.com>
2023-04-09 18:09:53 +0100

    Update MusicView.vue

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -1,13 +1,64 @@
 <template>
-    <div>
-      <h1>Hallo</h1>
-      <p>Welcome to the Music page!</p>
-    </div>
-  </template>
+  <div class="podcast-list">
+    <h2>Music</h2>
+    <ul>
+      <PodcastItem
+        v-for="podcast in podcasts"
+        :key="podcast.id"
+        :podcast="podcast"
+        @play="playPodcast"
+      />
+    </ul>
+    <MediaPlayer :podcast="selectedPodcast" :podcastList="podcasts" @play="playPodcast" />
+  </div>
+</template>
+
   
   <script>
+
+import PodcastItem from '@/components/PodcastItem.vue';
+import MediaPlayer from '@/components/MediaPlayer.vue';
+
   export default {
-    name: 'MusicView'
+    name: 'MusicView',
+    components: { PodcastItem, MediaPlayer },
+  data() {
+    return {
+      podcasts: [
+        {
+          id: 1,
+          title: "Checkie Brown - Roadtrip",
+          audioFilePath: "/audio/Checkie Brown - Roadtrip (CB 37).mp3",
+        },
+        {
+          id: 2,
+          title: "The Immaculates - Dogbite",
+          audioFilePath: "/audio/The Immaculates - Dogbite.mp3",
+        },
+        {
+          id: 3,
+          title: "Choklate -Incredible (Live @ KEXP)",
+          audioFilePath: "/audio/Choklate - Incredible (Live @ KEXP).mp3",
+        },
+        {
+          id: 4,
+          title: "Eastern Blokhedz - Live at Barbés",
+          audioFilePath: "/audio/Eastern Blokhedz - Live at Barbés, 12_10_2016.mp3",
+        },
+        {
+          id: 5,
+          title: "Wolf Asylum - Koord",
+          audioFilePath: "/audio/Wolf Asylum - Koord.mp3",
+        },
+      ],
+      selectedPodcast: null,
+    };
+  },
+  methods: {
+    playPodcast(podcast) {
+      this.selectedPodcast = podcast;
+    },
+  },
   };
   </script>
   
