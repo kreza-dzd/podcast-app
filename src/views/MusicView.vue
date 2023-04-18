68371ea8338b29726908e5abe461d88192f53563
Marko Krezic <markokrezic90@gmail.com>
2023-04-18 08:19:02 +0200

    Update MusicView.vue

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -20,38 +20,7 @@ import MediaPlayer from '@/components/MediaPlayer.vue';
   export default {
     name: 'MusicView',
     components: { PodcastItem, MediaPlayer },
-  data() {
-    return {
-      podcasts: [
-        {
-          id: 1,
-          title: "Checkie Brown - Roadtrip",
-          audioFilePath: "/audio/Checkie Brown - Roadtrip (CB 37).mp3",
-        },
-        {
-          id: 2,
-          title: "The Immaculates - Dogbite",
-          audioFilePath: "/audio/The Immaculates - Dogbite.mp3",
-        },
-        {
-          id: 3,
-          title: "Choklate -Incredible (Live @ KEXP)",
-          audioFilePath: "/audio/Choklate - Incredible (Live @ KEXP).mp3",
-        },
-        {
-          id: 4,
-          title: "Eastern Blokhedz - Live at Barbés",
-          audioFilePath: "/audio/Eastern Blokhedz - Live at Barbés, 12_10_2016.mp3",
-        },
-        {
-          id: 5,
-          title: "Wolf Asylum - Koord",
-          audioFilePath: "/audio/Wolf Asylum - Koord.mp3",
-        },
-      ],
-      selectedPodcast: null,
-    };
-  },
+
   methods: {
     playPodcast(podcast) {
       this.selectedPodcast = podcast;
