1994d9fef885844008f828f248394bd49cb38a33
Marko Krezic <markokrezic90@gmail.com>
2023-05-17 11:12:16 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,14 @@
         <span class="menu-line" :class="{ active: showSidebar }" />
       </button>
       <router-link to="/">
-        <h1 class="app-title" @click="showTable = false">My App</h1>
-
+        <h1 class="app-title" @click="showTable = false; clearInputField()">My App</h1>
       </router-link>
        <div class="user-profile">
      <img src="./assets/CD.jpg" alt="User Profile" />
       </div>
        <div class="search-container">
       <input type="text" placeholder="Search" v-model="searchQuery" @keyup.enter="requestTracks"/>
-      <LetsGo ref="letsgoComponent" @on-play-preview="setPreviewUrl" :search-query="searchQuery" @on-toggle-fullscreen="toggleFullscreen" :show-table="showTable" @request-tracks="requestTracksFromInput"/>
+      <LetsGo ref="letsgoComponent" @on-play-preview="setPreviewUrl" :search-query="searchQuery" @on-toggle-fullscreen="toggleFullscreen" :show-table="showTable" @request-tracks="requestTracksFromInput"   @show-table="showTable = true"/>
 
 
 
@@ -45,9 +44,10 @@
       </nav>
     </div>
     <main>
-      <router-view></router-view>
+      <router-view @play="handlePlay" @toggleFullscreen="toggleFullscreen"></router-view>
+
     </main>
-    <MediaPlayer ref="mediaPlayerComponent" :podcast="podcast" :audio-preview-url="audioPreviewUrl" @remove-media-player="hideMediaPlayer" />
+    <MediaPlayer ref="mediaPlayerComponent" :podcast="podcast" :audio-preview-url="audioPreviewUrl" :audio-player="audioElement" @remove-media-player="hideMediaPlayer" :accessToken="accessToken"/>
 
   </div>
 </template>
@@ -56,42 +56,61 @@
 import LetsGo from '@/components/LetsGo.vue';
 import MediaPlayer from "@/components/MediaPlayer.vue";
 export default {
-   components: { LetsGo, MediaPlayer, },
+  components: { LetsGo, MediaPlayer },
   data() {
     return {
-       showSidebar: false,
-       previewUrl: "",
-       podcast: null,
-       audioPreviewUrl: "",
-       searchQuery: "",
-       showTable: true,
-       showTableHeader: false,
-       audioElement: new Audio(),
-       podcastList: []
+      showSidebar: false,
+      previewUrl: "",
+      podcast: null,
+      audioPreviewUrl: "",
+      searchQuery: "",
+      showTable: true,
+      showTableHeader: false,
+      audioElement: new Audio(),
+      accessToken: null,
+      podcastList: []
     };
   },
   methods: {
+     handlePlay(playlist) {
+    this.setPreviewUrl(playlist.preview_url, playlist);  
+  },
+    clearInputField() {
+      this.searchQuery = "";
+    },
     toggleSidebar() {
       this.showSidebar = !this.showSidebar;
     },
     setPreviewUrl(url, podcast) {
-    this.audioPreviewUrl = url;
-    this.podcast = podcast;
+      this.audioPreviewUrl = url;
+      this.podcast = podcast;
     },
     async requestTracks() {
-    this.$refs.letsgoComponent.$emit("request-tracks");
-    this.showTableHeader = true;
-  },
-  toggleFullscreen() {
-    this.$refs.mediaPlayerComponent.toggleFullscreen();
-  },
-  hideMediaPlayer() {
-    this.audioPreviewUrl = "";
-    this.podcast = null;
-  },
-  requestTracksFromInput() {
-    this.$refs.letsgoComponent.requestTracks();
-  },
+      this.$refs.letsgoComponent.$emit("request-tracks");
+      this.showTableHeader = true;
+    },
+    toggleFullscreen() {
+      this.$refs.mediaPlayerComponent.toggleFullscreen();
+    },
+    hideMediaPlayer() {
+      this.audioPreviewUrl = "";
+      this.podcast = null;
+    },
+    requestTracksFromInput() {
+      this.$refs.letsgoComponent.requestTracks();
+    },
+    playSelectedPlaylist(previewUrl) {
+      if (!previewUrl) {
+        console.error('No preview URL available for this playlist');
+        return;
+      }
+      this.audioElement.pause();
+
+      this.audioElement.src = previewUrl;
+      this.audioElement.play();
+
+      this.setPreviewUrl(previewUrl, null);
+    },
   },
 };
 </script>
