617db3ceb44060046e6f59a0b8c691da438ca56b
Marko Krezic <markokrezic90@gmail.com>
2023-06-25 23:04:41 +0100

    Update MusicView.vue

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -1,38 +1,49 @@
 <template>
-  <div class="podcast-list">
-    <h2>Music</h2>
-    <ul>
-      <PodcastItem
-        v-for="podcast in podcasts"
-        :key="podcast.id"
-        :podcast="podcast"
-        @play="playPodcast"
-      />
-    </ul>
-    <MediaPlayer :podcast="selectedPodcast" :podcastList="podcasts" @play="playPodcast" />
-  </div>
-</template>
-
+    <div class="podcast-list">
+      <h2>Music</h2>
+      <ul>
+        <PodcastItem
+          v-for="podcast in podcasts"
+          :key="podcast.id"
+          :podcast="podcast"
+          @play="playPodcast"
+        />
+      </ul>
+      <MediaPlayer :podcast="selectedPodcast" :podcastList="podcasts" @play="playPodcast" :audioPlayer="audioPlayer"  />
+    </div>
+  </template>
   
+    
   <script>
-import PodcastItem from '@/components/PodcastItem.vue';
-import MediaPlayer from '@/components/MediaPlayer.vue';
+  import PodcastItem from '@/components/PodcastItem.vue';
+  import MediaPlayer from '@/components/MediaPlayer.vue';
   export default {
     name: 'MusicView',
-  mounted() {
-    this.$emit('show-media-player', false);
-    this.$emit('show-letsgo', false);
-  },
-  beforeUnmount() {
-    this.$emit('show-media-player', true);
-    this.$emit('show-letsgo', true);
-  },
-    components: { PodcastItem, MediaPlayer },
-
-  methods: {
-    playPodcast(podcast) {
-      this.selectedPodcast = podcast;
+    props: {
+      podcasts: Array,  // expecting an array prop named podcasts
     },
-  },
+    data() {
+      return {
+
+      selectedPodcast: null,
+      audioPlayer: new Audio(),
+      }
+    },
+    mounted() {
+      this.$emit('show-media-player', false);
+      this.$emit('show-letsgo', false);
+    },
+    beforeUnmount() {
+      this.$emit('show-media-player', true);
+      this.$emit('show-letsgo', true);
+    },
+    components: { PodcastItem, MediaPlayer },
+    methods: {
+      playPodcast(podcast) {
+        this.selectedPodcast = podcast;
+      },
+    },
   };
-  </script>
+  </script> 
+  
+  
