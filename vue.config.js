051f67d36839ac3f46fd31354d52016bb684b7d7
Marko Krezic <markokrezic90@gmail.com>
2023-07-13 22:36:03 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+  },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/podcast-app/" : "/",
-});
+};
